609a5a0a8f28262bddde22c14ffe39a2543b0f27
Junseo Jang <73162197+junseo-jang@users.noreply.github.com>
2026-03-14 15:14:03 +0900

    Fix typo in pip install command for requirements

--- math_grpo/setting.sh
+++ math_grpo/setting.sh
@@ -2,4 +2,4 @@
 
 pip install git+https://github.com/seopbo/nemo-skills-patch.git
 
-pip install -r requirements.txt --extra-index-url https://download.pytorch.org/whl/cu129ㄴ
+pip install -r requirements.txt --extra-index-url https://download.pytorch.org/whl/cu129
