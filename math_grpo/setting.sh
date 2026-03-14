e687678a5b45edf3a8a9c808461d946f99f6e7d2
Junseo Jang <73162197+junseo-jang@users.noreply.github.com>
2026-03-14 16:04:52 +0900

    Update setting.sh

--- math_grpo/setting.sh
+++ math_grpo/setting.sh
@@ -1,5 +1,17 @@
 #/usr/bin/env bash
 
-pip install git+https://github.com/seopbo/nemo-skills-patch.git
+echo "=== blinker 충돌 해결 및 nemo-evaluator-launcher 설치 ==="
+pip install --ignore-installed blinker
+pip install nemo-evaluator-launcher
 
-pip install -r requirements.txt --extra-index-url https://download.pytorch.org/whl/cu129
+echo "=== NeMo-Skills GitHub에서 클론 ==="
+cd /root
+
+git clone https://github.com/NVIDIA/NeMo-Skills.git
+
+
+echo "=== NeMo-Skills 설치 ==="
+cd NeMo-Skills
+pip install -e .
+
+pip install -r /root/open_RLFT/math_grpo/requirements.txt --extra-index-url https://download.pytorch.org/whl/cu129
